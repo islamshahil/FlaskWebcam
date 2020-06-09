08d5684964ff19b1e2caf7ab75f762af974b176c
vidhant <mattavidhant@gmail.com>
2020-06-09 23:24:21 +0530

    second

--- utils.py
+++ utils.py
@@ -5,9 +5,9 @@ import base64
 
 def pil_image_to_base64(pil_image):
     buf = BytesIO()
-    pil_image.save(buf, format="JPEG")
+    pil_image.save(buf, format="JPEG") #img to b64
     return base64.b64encode(buf.getvalue())
 
 
 def base64_to_pil_image(base64_img):
-    return Image.open(BytesIO(base64.b64decode(base64_img))) 
+    return Image.open(BytesIO(base64.b64decode(base64_img)))  #b64 to img
